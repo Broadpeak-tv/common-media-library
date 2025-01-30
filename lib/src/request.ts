b0f21a05d37d6203182a452430931cb15722f01a
Bertrand Berthelot <bertrand.berthelot@broadpeak.tv>
2025-01-30 11:18:00 +0100

    Update request exports

--- lib/src/request.ts
+++ lib/src/request.ts
@@ -5,6 +5,13 @@
  */
 export type { CommonMediaRequest } from './request/CommonMediaRequest.js';
 export type { CommonMediaResponse } from './request/CommonMediaResponse.js';
+export { FetchAbortedReason } from './request/FetchAbortedReason.js';
+export { FETCH_ABORTED_TIMEOUT } from './request/FETCH_ABORTED_TIMEOUT.js';
+export { FETCH_ABORTED_USER } from './request/FETCH_ABORTED_USER.js';
+export { FetchLoader } from './request/FetchLoader.js';
+export type { FetchLoaderOptions } from './request/FetchLoaderOptions.js';
 export type { RequestInterceptor } from './request/RequestInterceptor.js';
 export type { ResourceTiming } from './request/ResourceTiming.js';
 export type { ResponseInterceptor } from './request/ResponseInterceptor.js';
+export { ResponseType } from './request/ResponseType.js';
+
